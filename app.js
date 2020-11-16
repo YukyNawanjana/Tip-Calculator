86d4de61697a0b43966da67e794e49992d7d269c
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-11-16 22:12:38 +0530

    update js

---
+++ app.js
@@ -0,0 +1,3 @@
+const billAmount = document.getElementById('billAmount');
+const serviceQuality = document.getElementById('serviceQuality');
+const totalPeople = document.getElementById('totalPeople');
