71538811b3ceef3544546ac369102939ff793d3d
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-11-17 21:21:50 +0530

    update site

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const billAmount = document.getElementById('billAmount');
 const serviceQuality = document.getElementById('serviceQuality');
 const totalPeople = document.getElementById('totalPeople');
+const calculateBtn = document.getElementById('calBtn');
+
+
+calculateBtn.addEventListener('click', ()=>{
+   
+});
