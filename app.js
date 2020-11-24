842b2677b98b7011277a898f9604f5950a062169
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-11-24 22:07:57 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const billAmount = document.getElementById('billAmount').value;
 const serviceQuality = document.getElementById('serviceQuality').value;
 const totalPeople = document.getElementById('totalPeople').value;
 const calculateBtn = document.getElementById('calBtn');
-
+const tip = document.getElementById('tip');
 
 calculateBtn.addEventListener('click', ()=>{
 
